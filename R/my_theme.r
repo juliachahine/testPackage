febfb6689f94bfff955a4540e0341b1e12f797ac
juliachahine <jrchahine@gmail.com>
2024-03-03 13:21:50 -0500

    comment out test theme

--- R/my_theme.r
+++ R/my_theme.r
@@ -3,11 +3,11 @@
 #' 
 
 
-my_theme <- function(){
+my_theme <- function() {
     ggplot2::theme_bw(
         text = ggplot2::element_text(family = "CenturySch"),
-        ggplot2::scale_color_manual(values = list("#ED1C24", "#4E3629", "#FFC72C","#98A4AE","#59CBE8","#00B398","#003C71","#B7B09C")),
-        ggplot2::scale_fill_manual(values = list("#ED1C24", "#4E3629", "#FFC72C","#98A4AE","#59CBE8","#00B398","#003C71","#B7B09C")),
-        ggplot2::scale_fill_gradient(values = list("#ED1C24","#98A4AE")),
+        # ggplot2::scale_color_manual(values = list("#ED1C24", "#4E3629", "#FFC72C","#98A4AE","#59CBE8","#00B398","#003C71","#B7B09C")),
+        # ggplot2::scale_fill_manual(values = list("#ED1C24", "#4E3629", "#FFC72C","#98A4AE","#59CBE8","#00B398","#003C71","#B7B09C")),
+        # ggplot2::scale_fill_gradient(values = list("#ED1C24","#98A4AE")),
     )
 }
