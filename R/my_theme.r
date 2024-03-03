14b62bd22e51e0ca2923d553e29e14b97c69a3e1
juliachahine <jrchahine@gmail.com>
2024-03-03 13:15:43 -0500

    theme update

--- R/my_theme.r
+++ R/my_theme.r
@@ -4,8 +4,7 @@
 
 
 my_theme() <- function(){
-    ggplot2::theme_bw() + 
-    ggplot2::theme(
+    ggplot2::theme_bw(
         text = ggplot2::element_text(family = "CenturySch"),
         ggplot2::scale_color_manual(values = list("#ED1C24", "#4E3629", "#FFC72C","#98A4AE","#59CBE8","#00B398","#003C71","#B7B09C")),
         ggplot2::scale_fill_manual(values = list("#ED1C24", "#4E3629", "#FFC72C","#98A4AE","#59CBE8","#00B398","#003C71","#B7B09C")),
