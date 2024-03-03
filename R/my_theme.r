b2274a75558faaa7014ea050ea0b40a6e589d026
juliachahine <jrchahine@gmail.com>
2024-03-03 14:27:50 -0500

    trying theme

--- R/my_theme.r
+++ R/my_theme.r
@@ -8,11 +8,7 @@ my_theme <- function() {
     ggplot2::theme(
         text = ggplot2::element_text(family = "CenturySch"),
         line = ggplot2::element_line(color = "#4E3629"),
-        legend.frame = ggplot2::element_rect(color = "#ED1C24"),
-
-
-        # ggplot2::scale_color_manual(values = list("#ED1C24", "#4E3629", "#FFC72C","#98A4AE","#59CBE8","#00B398","#003C71","#B7B09C")),
-        # ggplot2::scale_fill_manual(values = list("#ED1C24", "#4E3629", "#FFC72C","#98A4AE","#59CBE8","#00B398","#003C71","#B7B09C")),
-        # ggplot2::scale_fill_gradient(low = "#98A4AE", high = "#ED1C24")
-        )
+        legend.frame = ggplot2::element_rect(color = "#ED1C24") 
+    )
+    +.gg(scale_color_manual(values = c("#ED1C24", "#4E3629", "#FFC72C","#98A4AE","#59CBE8","#00B398", "#003C71","#B7B09C")))
 }
