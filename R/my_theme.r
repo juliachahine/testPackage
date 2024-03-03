48bec15e7b13f1f2f7d41d9ca7ca2ac49703d7c7
juliachahine <jrchahine@gmail.com>
2024-03-03 14:31:55 -0500

    take out color attempt

--- R/my_theme.r
+++ R/my_theme.r
@@ -9,5 +9,5 @@ my_theme <- function() {
         text = ggplot2::element_text(family = "CenturySch"),
         line = ggplot2::element_line(color = "#4E3629"),
         legend.frame = ggplot2::element_rect(color = "#ED1C24") 
-    ) %+% scale_color_manual(values = c("#ED1C24", "#4E3629", "#FFC72C","#98A4AE","#59CBE8","#00B398", "#003C71","#B7B09C"))
+    ) 
 }
