1e04a0dec52e31a7c326f6dae74a47bfd468641f
juliachahine <jrchahine@gmail.com>
2024-03-03 15:04:54 -0500

    legend pos

--- R/my_theme.r
+++ R/my_theme.r
@@ -8,6 +8,7 @@ my_theme <- function() {
     ggplot2::theme(
     text = ggplot2::element_text(family = "CenturySch"),
     line = ggplot2::element_line(color = "#4E3629"),
+    legend.position="top",
     legend.frame = ggplot2::element_rect(color = "#ED1C24"),
     panel.background = ggplot2::element_rect(color = "#f4f3ef"),
     panel.grid.major = ggplot2::element_blank(),
