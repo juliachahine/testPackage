c2c4dba41ddfa7362966d2cbe8816c7d8ee741ce
juliachahine <jrchahine@gmail.com>
2024-03-03 13:33:37 -0500

    change color scale

--- R/my_theme.r
+++ R/my_theme.r
@@ -9,6 +9,6 @@ my_theme <- function() {
         text = ggplot2::element_text(family = "CenturySch"),
         ggplot2::scale_color_manual(values = list("#ED1C24", "#4E3629", "#FFC72C","#98A4AE","#59CBE8","#00B398","#003C71","#B7B09C")),
         ggplot2::scale_fill_manual(values = list("#ED1C24", "#4E3629", "#FFC72C","#98A4AE","#59CBE8","#00B398","#003C71","#B7B09C")),
-        ggplot2::scale_fill_gradient(values = list("#ED1C24","#98A4AE")),
-    )
+        ggplot2::scale_fill_gradient(low = "#98A4AE", high = "#ED1C24")
+        )
 }
