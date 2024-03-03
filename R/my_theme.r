ea2173a7fb504085dbb1deeaf8948b18b20e79f2
juliachahine <jrchahine@gmail.com>
2024-03-03 14:34:26 -0500

    fix

--- R/my_theme.r
+++ R/my_theme.r
@@ -4,7 +4,7 @@
 
 
 my_theme <- function() {
-    ggplot2::theme_bw() %+replace%
+    ggplot2::theme_bw() ggplot2::%+replace%
     ggplot2::theme(
         text = ggplot2::element_text(family = "CenturySch"),
         line = ggplot2::element_line(color = "#4E3629"),
