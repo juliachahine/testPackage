80bc74ce4a65827d81bca5e877a0dd624935a31f
juliachahine <jrchahine@gmail.com>
2024-03-03 14:57:40 -0500

    againagain

--- R/my_theme.r
+++ R/my_theme.r
@@ -4,13 +4,13 @@
 
 
 my_theme <- function() {
-    ggplot2::theme_bw() %+replace%
+    ggplot2::theme_bw() + 
     ggplot2::theme(
-    text = ggplot2:element_text(family = "CenturySch"),
-    line = ggplot2:element_line(color = "#4E3629"),
-    legend.frame = ggplot2:element_rect(color = "#ED1C24"),
-    panel.background = ggplot2:element_rect(color = "#f4f3ef"),
-    panel.grid.major = ggplot2:element_blank(),
-    panel.grid.minor = ggplot2:element_blank()) 
+    text = ggplot2::element_text(family = "CenturySch"),
+    line = ggplot2::element_line(color = "#4E3629"),
+    legend.frame = ggplot2::element_rect(color = "#ED1C24"),
+    panel.background = ggplot2::element_rect(color = "#f4f3ef"),
+    panel.grid.major = ggplot2::element_blank(),
+    panel.grid.minor = ggplot2::element_blank()) 
     
 }
