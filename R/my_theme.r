45d1c6f0919d3f2fc03f11f6cec6f8feb39ae28b
juliachahine <jrchahine@gmail.com>
2024-03-03 11:57:01 -0500

    my theme fix

--- R/my_theme.r
+++ R/my_theme.r
@@ -6,9 +6,9 @@
 my_theme <- function(){
     ggplot2::theme_bw() + 
     ggplot2::theme(
-        text = element_text(family = "CenturySch"),
-        scale_color_manual(values = list("#ED1C24", "#4E3629", "#FFC72C","#98A4AE","#59CBE8","#00B398","#003C71","#B7B09C")),
-        scale_fill_manual(values = list("#ED1C24", "#4E3629", "#FFC72C","#98A4AE","#59CBE8","#00B398","#003C71","#B7B09C")),
-        scale_fill_gradient(values = list("#ED1C24","#98A4AE")),
+        text = ggplot2::element_text(family = "CenturySch"),
+        ggplot2::scale_color_manual(values = list("#ED1C24", "#4E3629", "#FFC72C","#98A4AE","#59CBE8","#00B398","#003C71","#B7B09C")),
+        ggplot2::scale_fill_manual(values = list("#ED1C24", "#4E3629", "#FFC72C","#98A4AE","#59CBE8","#00B398","#003C71","#B7B09C")),
+        ggplot2::scale_fill_gradient(values = list("#ED1C24","#98A4AE")),
     )
 }
