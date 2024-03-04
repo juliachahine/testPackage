6c973b024d992087d1640fe363f0df0e1c6b1778
juliachahine <jrchahine@gmail.com>
2024-03-04 06:30:04 -0500

    last try on theme

--- R/my_theme.r
+++ R/my_theme.r
@@ -4,14 +4,14 @@
 
 
 my_theme <- function() {
-    ggplot2::theme_bw() + 
-    ggplot2::theme(
-    text = ggplot2::element_text(family = "CenturySch"),
-    line = ggplot2::element_line(color = "#4E3629"),
-    legend.position="top",
-    legend.frame = ggplot2::element_rect(color = "#ED1C24"),
-    panel.background = ggplot2::element_rect(color = "#f4f3ef"),
-    panel.grid.major = ggplot2::element_blank(),
-    panel.grid.minor = ggplot2::element_blank()) 
-    
+    thm <- ggplot2::theme_bw()
+    thm <- thm + ggplot2::theme(
+        text = ggplot2::element_text(family = "CenturySch", color = "#ED1C24"),
+        line = ggplot2::element_line(color = "#4E3629"),
+        legend.position="top",
+        legend.frame = ggplot2::element_rect(color = "#ED1C24"),
+        panel.background = ggplot2::element_rect(color = "#f4f3ef"),
+        panel.grid.major = ggplot2::element_blank(),
+        panel.grid.minor = ggplot2::element_blank()) 
+    return(thm)
 }
